eb298ed18627115a43a4d6786000afb9428ab30e
Bashkurova Ryshaniya <bashkurovar@mail.ru>
2021-01-10 19:19:18 +0300

    Initial Commit

---
+++ Emoji/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emoji
+//
+//  Created by Рушания on 10.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
